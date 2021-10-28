9e29c341fb9cd07bc54b16488e3be1858eec0b3d
baiama <primkulovbaiaman@gmail.com>
2021-10-28 16:48:24 +0600

    sada

--- ios/Classes/SwiftLocationPermissionPlugin.swift
+++ ios/Classes/SwiftLocationPermissionPlugin.swift
@@ -9,19 +9,9 @@ public class SwiftLocationPermissionPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-//      if(call.method == "request_permission"){
-//            LocationManager.shared.requestLocationAuthorization(completion: { status in
-//                result(status)
-//            })
-//      }else if(call.method == "open_settings"){
-//          LocationManager.shared.openSettings()
-//          result("not_always")
-//      }else {
-//          result(FlutterMethodNotImplemented)
-//      }
       switch call.method {
       case "request_permission":
-          LocationManager.shared.requestLocationWhileUse(completion: { status in
+          LocationManager.shared.requestLocationAuthorization(completion: { status in
               result(status)
           })
           break
